c79dfa5ae699b53e71323c60318374cb41b9b8e0
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-05-29 21:18:59 +0200

    init

---
+++ src/main/java/com/ashkiano/customtntdelay/CustomTNTDelay.java
@@ -0,0 +1,42 @@
+package com.ashkiano.customtntdelay;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomTNTDelay extends JavaPlugin implements Listener {
+
+    private int tntDelay; // delay in ticks
+
+    @Override
+    public void onEnable() {
+        // Load the configuration
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        int delayInSeconds = config.getInt("tnt-delay-seconds", 4);
+        tntDelay = delayInSeconds * 20; // convert seconds to ticks
+
+        // Register the event listener
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("CustomTNTDelay enabled with TNT delay of " + delayInSeconds + " seconds.");
+        Metrics metrics = new Metrics(this, 22004);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("CustomTNTDelay disabled!");
+    }
+
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        if (event.getEntityType() == EntityType.PRIMED_TNT) {
+            TNTPrimed tnt = (TNTPrimed) event.getEntity();
+            tnt.setFuseTicks(tntDelay);
+        }
+    }
+}
